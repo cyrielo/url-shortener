1c6453e6ff3c85598ddb5eea671aa1660bc670e0
Cyril Ologho <cyril.paul@andela.com>
2019-04-21 18:43:17 +0100

    add icons for marketing

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Row, Col, Card } from 'react-bootstrap';
-import RightSideBar from '../../components/RightSidebar/RightSidebar';
+import Mock1 from '../../assets/img/mock1.png';
+import Mock2 from '../../assets/img/mock2.png';
+import Mock3 from '../../assets/img/mock3.png';
+
+
 import './Home.scss';
 const Home = props => {
   return (
@@ -10,25 +14,27 @@ const Home = props => {
             <Card className="border-0 my-5 w-75 bg-transparent">
               <Card.Body className="p-0">
                 <div className="link-shadow text-center mb-5 shadow-sm p-2 rounded-circle d-inline-block">
-                  <i class="zmdi zmdi-8tracks"></i>
+                  <i className="zmdi zmdi-8tracks"></i>
                 </div>
                 <div className="promo-head">
                   <h2>
                     A simple link but a powerful tool 
                     for <strike>Youtubers.</strike>
                   </h2>
-                  <p>
-                    Our tool allows you to seamlessly track your audience with simple and 
-                    easy-to-remember yet powerful links and provide your customers a unique tailored experience.
-                  </p>
+                  <section className="mt-4">
+                    <p>
+                      Our tool allows you to seamlessly track your audience with simple and 
+                      easy-to-remember yet powerful links and provide your customers a unique tailored experience.
+                    </p>                    
+                  </section>
                 </div>
                 <div className="input-area mt-5">
-                  <div class="input-group mb-3 shadow p-3">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text bg-white border-0" id="basic-addon1"><i class="zmdi zmdi-8tracks"></i></span>
+                  <div className="input-group mb-3 shadow p-3">
+                    <div className="input-group-prepend">
+                      <span className="input-group-text bg-transparent border-0" id="basic-addon1"><i className="zmdi zmdi-8tracks"></i></span>
                     </div>
-                    <input type="url" class="form-control border-0" placeholder="https://youtube.com/watch?v=qidysnts" aria-label="url" aria-describedby="basic-addon1"/>
-                    <button className="btn text-white shorten-btn px-4"><i class="zmdi zmdi-copy"></i> Copy</button>
+                    <input type="url" className="form-control bg-transparent border-0" placeholder="https://youtube.com/watch?v=qidysnts" aria-label="url" aria-describedby="basic-addon1"/>
+                    <button className="btn text-white shorten-btn px-4"><i className="zmdi zmdi-copy"></i> Copy</button>
                   </div>
                   
                 </div>
@@ -40,32 +46,35 @@ const Home = props => {
           </Col>
         <Col className="" lg="6" >
           <div className="d-flex justify-content-end marketing-cards my-5">
-            <Card className="border-0 m-2 shadow p-3 w-50 marketing-card float-right">
+            <Card className="border-0 m-2 shadow px-3 pt-3 w-50 marketing-card float-right">
               <Card.Body className="p-0">
-                <strong>Measure. Optimize</strong>
-                <p>
-                  Use data to optimize your marketing campaing's performance.
+                <img src={Mock1} className="img-fluid d-block mb-2" />
+                <strong>Social. Reach</strong>
+                <p className="p-0">
+                  Share your link to your network.
                 </p>
               </Card.Body>
             </Card>
           </div>
 
           <div className="d-flex justify-content-center marketing-cards mb-5">
-            <Card className="border-0 m-2 shadow p-3 w-50 marketing-card float-right">
+            <Card className="border-0 m-2 shadow px-3 pt-3 w-50 marketing-card float-right">
                 <Card.Body className="p-0">
+                  <img src={Mock2} className="img-fluid d-block mb-2" />
                   <strong>Measure. Optimize</strong>
-                  <p>
-                    Share your link to your network.
+                  <p className="p-0">
+                    Use data to optimize your marketing campaing's performance.
                   </p>
                 </Card.Body>
               </Card>
           </div>
 
           <div className="d-flex justify-content-end marketing-cards mb-5">
-            <Card className="border-0 m-2 shadow p-3 w-50 marketing-card float-right">
+            <Card className="border-0 m-2 shadow px-3 pt-3 w-50 marketing-card float-right">
               <Card.Body className="p-0">
-                <strong>Measure. Optimize</strong>
-                <p> Target an audience that fits your needs.</p>
+                <img src={Mock3} className="img-fluid d-block mb-2" />
+                <strong>Target. Re-target</strong>
+                <p className="p-0"> Target an audience that fits your needs.</p>
               </Card.Body>
             </Card>
           </div>
