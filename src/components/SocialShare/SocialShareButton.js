245585154f624ab36b2c525ac0ebe2b3749774cb
Cyril Ologho <cyril.paul@andela.com>
2019-04-23 19:22:05 +0100

    add visual effect to Sharebutton

--- src/components/SocialShare/SocialShareButton.js
+++ src/components/SocialShare/SocialShareButton.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import urlHelper from '../../helpers/urlHelper';
+import './SocialShareButton.scss';
 
 const SocialShareButton = props => {
   const { url } = props;
   return (
     <div className="url-options mt-4">
-      <ul className="d-inline-block social-share bg-diabled text-white p-3">
-        <li className="d-inline-block mr-5">
+      <ul className={`d-inline-block social-share ${ url ? '' : 'bg-diabled'} text-white p-2`}>
+        <li className={`d-inline-block  ${url ? 'mr-5' : ''}`}>
           <i className="zmdi zmdi-share"></i>
         </li >
-        <li className="d-inline-block mr-2">
-          <a target="_blank" rel="noopener noreferrer" href={urlHelper.shareViaEmail(url)} className="text-white">
-            <i className="zmdi zmdi-email"></i>
-          </a>
-        </li >
-        <li className="d-inline-block mr-2">
-          <a target="_blank" rel="noopener noreferrer" href={urlHelper.shareViaFacebook(url)} title="Facebook Share" className="text-white">
-            <i className="zmdi zmdi-facebook"></i>
-          </a>
-        </li>
-        <li className="d-inline-block mr-2">
-          <a target="_blank" rel="noopener noreferrer" href={urlHelper.shareViaTwitter(url)} className="zmdi zmdi-twitter text-white"></a>
-        </li>
+        {
+          url ? (
+            <>
+              <li className="d-inline-block mr-2">
+                <a target="_blank" rel="noopener noreferrer" href={urlHelper.shareViaEmail(url)} className="text-white">
+                  <i className="zmdi zmdi-email"></i>
+                </a>
+              </li >
+              <li className="d-inline-block mr-2">
+                <a target="_blank" rel="noopener noreferrer" href={urlHelper.shareViaFacebook(url)} title="Facebook Share" className="text-white">
+                  <i className="zmdi zmdi-facebook"></i>
+                </a>
+              </li>
+              <li className="d-inline-block mr-2">
+                <a target="_blank" rel="noopener noreferrer" href={urlHelper.shareViaTwitter(url)} className="zmdi zmdi-twitter text-white"></a>
+              </li>            
+            </>
+          ): null
+        }
       </ul>
     </div>
   )
